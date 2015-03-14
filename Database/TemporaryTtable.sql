1b43811b6aa4739d2ada1fb0d360f6b1ecdfe6c5
Nelson Ragel <nelsonragel@users.noreply.github.com>
2015-03-14 07:24:35 -0400

    Create TemporaryTtable.sql

---
+++ Database/TemporaryTtable.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE `temp` (
+  `CourseCode` varchar(10) DEFAULT NULL,
+  `CourseName` varchar(255) DEFAULT NULL,
+  `Professor` varchar(255) DEFAULT NULL,
+  `Room` varchar(10) DEFAULT NULL,
+  `TimeSlot` varchar(255) DEFAULT NULL,
+  `DaySlot` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
